a0115cf7a1422ca1757eaed32ce6bb9008bb14fe
antoinco <antoinco@student.21-school.ru>
2024-04-14 23:57:35 +0300

    fix cache disable(

--- src/main/java/gazzsha/com/bannerservice/confuguration/cache/RedisConfiguration.java
+++ src/main/java/gazzsha/com/bannerservice/confuguration/cache/RedisConfiguration.java
@@ -1,59 +1,59 @@
-package gazzsha.com.bannerservice.confuguration.cache;
-
-import gazzsha.com.bannerservice.confuguration.property.RedisProperty;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
-
-@Configuration
-@RequiredArgsConstructor
-public class RedisConfiguration {
-
-    private final RedisProperty redisProperty;
-
-
-    @Bean
-    public JedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(
-                redisProperty.getHost(), redisProperty.getPort());
-        config.setPassword(redisProperty.getPassword());
-        return new JedisConnectionFactory(config);
-    }
-
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new JdkSerializationRedisSerializer());
-        template.setHashValueSerializer(new JdkSerializationRedisSerializer());
-        template.setEnableTransactionSupport(true);
-        template.afterPropertiesSet();
-        return template;
-    }
-
-    @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        RedisCacheConfiguration fiveMinuteTtlExpirationDefaults =
-                RedisCacheConfiguration.defaultCacheConfig()
-                        .entryTtl(Duration.ofMinutes(5))
-                        .disableCachingNullValues();
-
-        return RedisCacheManager.builder(connectionFactory)
-                .cacheDefaults(fiveMinuteTtlExpirationDefaults)
-                .transactionAware()
-                .build();
-    }
-}
+//package gazzsha.com.bannerservice.confuguration.cache;
+//
+//import gazzsha.com.bannerservice.confuguration.property.RedisProperty;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.cache.RedisCacheConfiguration;
+//import org.springframework.data.redis.cache.RedisCacheManager;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//import java.time.Duration;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class RedisConfiguration {
+//
+//    private final RedisProperty redisProperty;
+//
+//
+//    @Bean
+//    public JedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(
+//                redisProperty.getHost(), redisProperty.getPort());
+//        config.setPassword(redisProperty.getPassword());
+//        return new JedisConnectionFactory(config);
+//    }
+//
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(redisConnectionFactory());
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setHashKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new JdkSerializationRedisSerializer());
+//        template.setHashValueSerializer(new JdkSerializationRedisSerializer());
+//        template.setEnableTransactionSupport(true);
+//        template.afterPropertiesSet();
+//        return template;
+//    }
+//
+//    @Bean
+//    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+//        RedisCacheConfiguration fiveMinuteTtlExpirationDefaults =
+//                RedisCacheConfiguration.defaultCacheConfig()
+//                        .entryTtl(Duration.ofMinutes(5))
+//                        .disableCachingNullValues();
+//
+//        return RedisCacheManager.builder(connectionFactory)
+//                .cacheDefaults(fiveMinuteTtlExpirationDefaults)
+//                .transactionAware()
+//                .build();
+//    }
+//}
